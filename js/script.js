11772277feb93c489a73a7997a545e659738c8ef
MrArcano <salvatore-dario@hotmail.it>
2023-09-19 12:19:16 +0200

    [add]: view all bomb

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const btnGen = document.getElementById("btn-gen");
 const containerBox = document.getElementById("container-box");
 const dimBox = document.getElementById("input-dim");
 let counterPoint = 0;
+let arrayBomb = [];
+let countBox = 0;
 
 addOptionSelect();
 
@@ -17,15 +19,15 @@ btnGen.addEventListener("click",function(){
 // FUNCTION CORE APP
 function init(){
     // seleziono dimBox
-    const countBox = dimBox.value ** 2;
+    countBox = dimBox.value ** 2;
     // --------------------------------------------------------------------
     // genero casualmente la posizione delle bombe da min a max, ultimo numero è la quantità di bombe in % -----> 0.16 = 16%
-    const arrayBomb = randomizerUnique(1,countBox,countBox*0.16);
+    arrayBomb = randomizerUnique(1,countBox,countBox*0.16);
     // --------------------------------------------------------------------
     // 2. Add box
     for(let i=1; i<=countBox; i++){
       // richiamo la creazione di un box
-      const box = createBox(i,dimBox.value,arrayBomb);
+      const box = createBox(i);
 
       // append del box, al containerBox
       containerBox.append(box);
@@ -33,32 +35,30 @@ function init(){
 };
 
 // FUNCTION CREA BOX
-function createBox(index,dimBox,arrayBomb){
+function createBox(index){
   const newBox = document.createElement("div");
   newBox.className = "box";
-  newBox.style.width = 100 / dimBox + "%";
+  newBox.style.width = 100 / dimBox.value + "%";
   newBox._boxID = index;
-  newBox.addEventListener("click",function(){
-    handlerBox(newBox,arrayBomb);
-  });
+  newBox.addEventListener("click",handlerBox);
   return newBox;
 };
 
 // FUNCTION HANDLERBOX
-function handlerBox(box,arrayBomb){
+function handlerBox(){
   // rimuovo l'eventListener del click
-  // this.removeEventListener("click",handlerBox);
+  this.removeEventListener("click",handlerBox);
   // console.dir(this);
-  console.log("iao");
-  console.log(box._boxID);
+  console.log("boxID: ",this._boxID);
   // aggiungo la classe clicked o clicked-bomb per il cambio del bg
   if(!(arrayBomb.includes(this._boxID))){
     this.classList.add("clicked");
     counterPoint++;
   }else{
     this.classList.add("clicked-bomb");
-    finegioco();
+    endGame();
   }
+  return this;
 }
 
 // FUNCTION ADD OPTION SELECT
@@ -101,4 +101,21 @@ function randomizerUnique(min,max,dim){
     console.log("Errore! la dimensione del vettore finale, deve essere inferiore alla quantità di numeri tra min e max");
   }
   return randArray;
+}
+
+// FUNCTION FINE GIOCO
+function endGame(){
+  viewBomb();
+  console.log("Hai totalizzato: " + counterPoint + " punti")
+  console.log("Ti è esplosa una bomba tra le mani!");
+}
+
+// FUNCTION viewBomb
+function viewBomb(){
+  const allBox = document.getElementsByClassName("box");
+  for( i = 0 ; i < allBox.length; i++){
+    if(arrayBomb.includes(allBox[i]._boxID)){
+      allBox[i].classList.add("clicked-bomb");
+    }
+  }
 }
